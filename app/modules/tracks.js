6c810a5df3457f6a137a442684f41e4b63ec6204
eddiekennedy <eddiekennedy@gmail.com>
2012-09-25 22:37:07 -0400

    Updated thumbnail title

--- app/modules/tracks.js
+++ app/modules/tracks.js
@@ -44,8 +44,11 @@ function(app) {
       response = _.each( response, function( track ){
         track.indexNum = response.indexOf(track);
         if ( track && track.artwork_url ) {
-          // Replace default image artwork url with larger version
+          // Save tiny version for thumbnail
           track.icon = track.artwork_url.replace( "large", "tiny" );
+          // Set title for thumbnail
+          track.titleAttr = track.title + " | " + track.user.username;
+          // Replace default image artwork url with larger version
           track.artwork_url = track.artwork_url.replace( "large", "t500x500" );
         } 
         return track;
